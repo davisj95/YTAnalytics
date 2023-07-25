c61468ac54f51ae293466fe31fdcddb98e0a31d7
davisj95 <jacobrdavis95@gmail.com>
2023-07-25 14:54:14 -0600

    Added functionality to query data split by live & on demand

---
+++ R/live_onDemand.R
@@ -0,0 +1,77 @@
+
+
+#' Video Live & OnDemand Data
+#' 
+#' Return video data split by live & on demand.
+#'
+#' @param videoId Required.String. Id of YouTube video.
+#' @param ... Addt. arguments passed to \code{analytics_request}
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' video_live_onDemand(videoId = "dQw4w9WgXcQ")
+#' }
+
+video_live_onDemand <- function(videoId = NULL, ...) {
+  temp <- analytics_request(dimensions = "liveOrOnDemand",
+                            metrics = "views,estimatedMinutesWatched,averageViewDuration",
+                            filters = paste0("video==", videoId), ...)
+  return(temp)
+}
+
+
+
+
+
+
+
+#' Playlist Live & OnDemand Data
+#' 
+#' Return playlist data split by live & on demand.
+#'
+#' @param playlistId Required. String. Id of YouTube playlist.
+#' @param ... Addt. arguments passed to \code{analytics_request}
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' playlist_live_onDemand(playlistId = "PL2MI040U_GXq1L5JUxNOulWCyXn-7QyZK")
+#' }
+
+playlist_live_onDemand <- function(playlistId = NULL, ...) {
+  temp <- analytics_request(dimensions = "liveOrOnDemand",
+                            metrics = "views,estimatedMinutesWatched,averageViewDuration",
+                            filters = paste0("playlist==", playlistId, ";isCurated==1"), ...)
+  return(temp)
+}
+
+
+
+
+
+
+
+#' Channel Live & OnDemand Data
+#' 
+#' Return channel data split by live & on demand.
+#'
+#' @param ... Addt. arguments passed to \code{analytics_request}
+#'
+#' @return data.frame()
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' channel_live_onDemand()
+#' }
+
+channel_live_onDemand <- function(...) {
+  temp <- analytics_request(dimensions = "liveOrOnDemand",
+                            metrics = "views,estimatedMinutesWatched,averageViewDuration", ...)
+  return(temp)
+}
