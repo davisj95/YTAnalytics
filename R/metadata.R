a349184aa0f0ca8a06affe5a6c5e4d6d3a4614c3
davisj95 <jacobrdavis95@gmail.com>
2024-08-23 11:10:38 -0600

    Added playlist metadata function

--- R/metadata.R
+++ R/metadata.R
@@ -25,3 +25,43 @@ video_metadata <- function(videoId = NULL, ...) {
   }
   return(metadata)
 }
+
+
+
+#' Playlist Metadata
+#'
+#' @param videoId Required. String. Id of YouTube Video.
+#' @param ... Addt. arguments passed to \code{data_video_request}
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' playlist_metadata(playlistId = "dQw4w9WgXcQ")
+#' }
+
+playlist_metadata <- function(playlistId = NULL, ...) {
+  
+  metadata <- data.frame()
+  for(i in 1:length(playlistId)) {
+    temp <- as.data.frame(data_playlist_request(part = "snippet", id = playlistId[i], ...))
+    if(!is.null(temp)) {
+      temp2 <- temp$items.snippet
+      metadata <- dplyr::bind_rows(metadata, error_checking(temp2, playlistId[i], "playlist"))
+    }
+  }
+  return(metadata)
+}
+test <- playlist_metadata(playlistId, token = newEnglishToken)
+
+videoId <- "DHBo7ka3YZQ"
+
+YTAnalytics::video_countries(videoId, metrics <- "views,estimatedMinutesWatched,subscribersGained", token = newEnglishToken)
+
+YTAnalytics::video_traffic_details(videoId, metrics <- "views,estimatedMinutesWatched,subscribersGained", token = newEnglishToken)
+
+testError <- "Invalid value (views,estimatedMinutesWatched,subscribersGained) given in field parameters.filters.globalinvalid"
+pattern <- "Invalid value \\(.*\\) given in field parameters\\.filters\\.globalinvalid"
+
+if (grepl(pattern, testError))
