c0ce121848fd0fab4abc59fc71c542a45f943789
davisj95 <jacobrdavis95@gmail.com>
2024-08-28 13:24:29 -0600

    Add progress bar

--- R/live_onDemand.R
+++ R/live_onDemand.R
@@ -19,7 +19,9 @@
 video_live_onDemand <- function(videoId = NULL, ...) {
   
   results <- data.frame()
+  pb <- prog_bar(length(videoId))
   for(i in 1:length(videoId)) {
+    pb$tick()
     temp <- analytics_request(dimensions = "liveOrOnDemand",
                               filters = paste0("video==", videoId[i]), ...)
     
@@ -49,7 +51,9 @@ video_live_onDemand <- function(videoId = NULL, ...) {
 playlist_live_onDemand <- function(playlistId = NULL, ...) {
   
   results <- data.frame()
+  pb <- prog_bar(length(playlistId))
   for(i in 1:length(playlistId)) {
+    pb$tick()
     temp <- analytics_request(dimensions = "liveOrOnDemand",
                               filters = paste0("playlist==", playlistId[i], ";isCurated==1"), ...)
     
