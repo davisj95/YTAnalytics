53d94a0433c4a08dce5808a4630b206102a3c825
davisj95 <jakealthor+github@proton.me>
2025-03-15 09:26:26 -0600

    Bug fix

--- R/utils.R
+++ R/utils.R
@@ -59,7 +59,7 @@ analytics_request <- function(dimensions = NULL,
     params[["sort"]] <- paste0("-",sub("\\,.*","", metrics))
   }
   
-  paramsList <- Filter(Negate(is.null), params[params != "token"])
+  paramsList <- Filter(Negate(is.null), params[names(params) != "token"])
   
   queryUrl <- paste0(baseUrl, paste0("&", names(paramsList), "=", unlist(paramsList), collapse = ""))
   
@@ -295,7 +295,7 @@ youtube_GET <- function(url = NULL, request = NULL, token){
   
   if(is.null(url) | is.null(request)) stop("url and request required.")
   
-  r <- httr::GET(url, token, httr::config(http_version=2))
+  r <- httr::GET(url, token)
   temp <- jsonlite::fromJSON(httr::content(r, as = "text"))
   
   if(httr::status_code(r) != 200) stop(temp$error$errors)
